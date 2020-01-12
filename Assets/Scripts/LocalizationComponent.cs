8760d70a0c1418741592c56867eeb1ea9745bd36
unknown <denisb41@gmail.com>
2020-01-12 20:18:49 +0200

    loca basics

---
+++ Assets/Scripts/LocalizationComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+public class LocalizationComponent : MonoBehaviour
+{
+    public string key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // find localization manager in globals
+        // scan current for text field
+        // replace by provided value
+        GameObject globals = GameObject.Find("Globals");
+        Assert.IsNotNull(globals, "Globals GameObject not found!");
+        LocalizationManager locaMgr = globals.GetComponent<LocalizationManager>();
+        Assert.IsNotNull(locaMgr, "LocalizationManager not found!");
+        Text goText = GetComponent<Text>();
+        goText.text = locaMgr.GetText(key);
+    }
+}
---
+++ Assets/Scripts/LocalizationManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// load loca file
+//make singleton
+[System.Serializable]
+public class LocaEntry
+{
+    public string key;
+    public string en;
+    public string ru;
+}
+
+[System.Serializable]
+public class LocaTexts
+{
+    public LocaEntry[] entries;
+}
+
+public class LocalizationManager : MonoBehaviour
+{
+    private string locale;
+    private Dictionary<string, Dictionary<string, string>> dicts = new Dictionary<string, Dictionary<string, string>>();
+    private Dictionary<string, string> activeDict;
+    public TextAsset jsonFile;
+
+    void LoadTexts()
+    {
+        //hardcoding locales looks kinda ugly, but with Unity Json handling it's simplest way
+        dicts.Add("en", new Dictionary<string, string>());
+        dicts.Add("ru", new Dictionary<string, string>());
+
+        LocaTexts textsInJson = JsonUtility.FromJson<LocaTexts>(jsonFile.text);
+
+        foreach (LocaEntry e in textsInJson.entries)
+        {
+            //Debug.Log("Found entry: " + e.key + " " + e.en);
+            dicts["en"].Add(e.key, e.en);
+            dicts["ru"].Add(e.key, e.ru);
+        }
+    }
+
+    // Awake is always called before any Start function
+    void Awake()
+    {
+        LoadTexts();
+        SetLocale("en"); //get from OS
+    }
+
+    public void SetLocale(string newLocale)
+    {
+        if(dicts.TryGetValue(newLocale, out activeDict))
+        {
+            locale = newLocale;
+        }
+        else
+        {
+            Debug.Log("Locale not found: " + newLocale);
+        }
+    }
+
+    public string GetText(string key)
+    {
+        string result = key + "_localization_not_found";
+        if (activeDict != null)
+        {
+            activeDict.TryGetValue(key, out result);
+        }
+
+        return result;
+    }
+}
